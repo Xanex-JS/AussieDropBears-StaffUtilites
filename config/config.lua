418befea55dd7069e8cc1abb7c00e52919bcefe5
Xanex <mackacouzner0@gmail.com>
2022-07-02 13:54:47 +0800

    Edit code

--- config/config.lua
+++ config/config.lua
@@ -5,5 +5,5 @@ Config.Prefix = '^1[^5Staff Yeet^1] ^3'
 
 -- Default Ped Model Pemrissions bypass
 
-Config.AllowBypass = true
-Config.Bypass = "Discord" -- Ace = Ace Permissions -- Discord = Discord Permisisons
+Config.AllowBypass = true -- Coming soon
+Config.Bypass = "Discord" -- Coming soon
