ae7833df71d46bbe08bb30c582c87ca5d2be4a02
Xanex <mackacouzner0@gmail.com>
2022-07-02 14:54:55 +0800

    Removed double line of code :/

--- display.lua
+++ display.lua
@@ -2,7 +2,6 @@ Citizen.CreateThread(function()
   if (GetCurrentResourceName() ~= "AussieDropBears-StaffUtilites") then
 		StopResource(GetCurrentResourceName());
 		print("[" .. GetCurrentResourceName() .. "] " .. "IMPORTANT: This resource must be named AussieDropBears-StaffUtilites for it to work properly with other scripts...");
-    StopResource(GetCurrentResourceName());
 	end
 	print("[AussieDropBears-StaffUtilites] For support, make sure to join Aspire's official Discord server: discord.gg/RRPEHr3Z2q");
     if GetCurrentResourceName() == 'AussieDropBears-StaffUtilites' then
